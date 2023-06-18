de27f7642caa7e3966b765b6739ce7b539c43350
RichyRed <richyred1090@gmail.com>
2023-06-18 18:35:00 -0400

    cosas

--- appTablero/src/app/components/TableReport.tsx
+++ appTablero/src/app/components/TableReport.tsx
@@ -1,78 +1,127 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import * as React from "react";
+import { useState } from "react";
+import { Grid, Paper, Typography, TextField, Button } from "@mui/material";
+import { styled } from "@mui/system";
+import { Add } from "@mui/icons-material";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
+interface Task {
+  id: number;
+  title: string;
+  description: string;
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+interface Column {
+  id: number;
+  name: string;
+  tasks: Task[];
+}
 
-export default function TableReport() {
+const ColumnContainer = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  marginBottom: theme.spacing(2),
+}));
+
+const TaskCard = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  marginBottom: theme.spacing(1),
+}));
+
+const TableReport = () => {
+  const [columns, setColumns] = useState<Column[]>([
+    { id: 1, name: "Columna 1", tasks: [] },
+    { id: 2, name: "Columna 2", tasks: [] },
+    { id: 3, name: "Columna 3", tasks: [] },
+  ]);
+
+  const handleAddTask = (columnId: number) => {
+    const taskTitle = prompt("Ingrese el título de la tarea");
+    const taskDescription = prompt("Ingrese la descripción de la tarea");
+
+    if (taskTitle && taskDescription) {
+      const newTask: Task = {
+        id: Date.now(),
+        title: taskTitle,
+        description: taskDescription,
+      };
+
+      const updatedColumns = columns.map((column) => {
+        if (column.id === columnId) {
+          return {
+            ...column,
+            tasks: [...column.tasks, newTask],
+          };
+        }
+        return column;
+      });
+
+      setColumns(updatedColumns);
+    }
+  };
+
+  const handleDeleteTask = (columnId: number, taskId: number) => {
+    const updatedColumns = columns.map((column) => {
+      if (column.id === columnId) {
+        const updatedTasks = column.tasks.filter((task) => task.id !== taskId);
+        return {
+          ...column,
+          tasks: updatedTasks,
+        };
+      }
+      return column;
+    });
+
+    setColumns(updatedColumns);
+  };
+
+  const handleUpdateColumnName = (columnId: number) => {
+    const columnName = prompt("Ingrese el nuevo nombre de la columna");
+
+    if (columnName) {
+      const updatedColumns = columns.map((column) => {
+        if (column.id === columnId) {
+          return {
+            ...column,
+            name: columnName,
+          };
+        }
+        return column;
+      });
+
+      setColumns(updatedColumns);
+    }
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Grid container spacing={2}>
+      {columns.map((column) => (
+        <Grid item xs={4} key={column.id}>
+          <ColumnContainer elevation={3}>
+            <Typography variant="h6">{column.name}</Typography>
+            <Button size="small" onClick={() => handleUpdateColumnName(column.id)}>
+              Editar nombre
+            </Button>
+            <Button size="small" onClick={() => handleAddTask(column.id)}>
+              <Add />
+              Añadir tarea
+            </Button>
+            {column.tasks.map((task) => (
+              <TaskCard key={task.id} elevation={2}>
+                <Typography variant="subtitle1">{task.title}</Typography>
+                <Typography variant="body2">{task.description}</Typography>
+                <Button
+                  size="small"
+                  color="error"
+                  onClick={() => handleDeleteTask(column.id, task.id)}
+                >
+                  Eliminar
+                </Button>
+              </TaskCard>
+            ))}
+          </ColumnContainer>
+        </Grid>
+      ))}
+    </Grid>
   );
-}
+};
+
+export default TableReport;
